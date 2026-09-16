3ceceb91bc6a773e2e661f46d7cd9d2e559990f6
MicaLutzDG <smicadg13@gmail.com>
2026-09-16 06:41:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const glow = document.querySelector(".cursor-glow");
+window.addEventListener("pointermove", e => {
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+});
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    const target = document.querySelector(link.getAttribute("href"));
+    if (target) { e.preventDefault(); target.scrollIntoView({behavior:"smooth"}); }
+  });
+});
